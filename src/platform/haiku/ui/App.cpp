d8e2c2eb7914a9c5d27599b7c9d5f356eb807f3c
Tara Harris <3769985+realtaraharris@users.noreply.github.com>
2023-12-13 23:22:31 -0500

    Haiku: support file loading from the command line

--- src/platform/haiku/ui/App.cpp
+++ src/platform/haiku/ui/App.cpp
@@ -1,9 +1,26 @@
 #include "App.h"
 #include "MainWindow.h"
 
+#include <iostream>
+
 App::App(void) : BApplication("application/x-vnd.dw-TestApp") {
     MainWindow *mainwin = new MainWindow();
     mainwin->Show();
+}
+
+void BApplication::ArgvReceived(int32 argc, char **argv) {
+	if (argc > 0) {
+		BMessage *msg = new BMessage(READ_FILE);
+		entry_ref ref;
+		get_ref_for_path(argv[1], &ref);
+
+		if (msg->AddRef("refs", &ref) != B_OK) {
+			std::cerr << "error parsing args in BApplication::ArgvReceived" << std::endl;
+			return;
+		}
+
+		be_app->WindowAt(MAIN_WINDOW)->PostMessage(msg);
+	}
 }
 
 int main(void) {
