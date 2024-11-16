d270cbb930ec5e0f3c71ed8c0e56a20966743b93
Tara Harris <3769985+realtaraharris@users.noreply.github.com>
2024-11-16 10:45:37 -0800

    cpp2: move Request class definition into request.h

--- src/sketch.h
+++ src/sketch.h
@@ -139,56 +139,7 @@ struct EntityKeyEqual {
 typedef std::unordered_map<EntityKey, EntityId, EntityKeyHash, EntityKeyEqual> EntityMap;
 
 #include "group.h"
-
-// A user request for some primitive or derived operation; for example a
-// line, or a step and repeat.
-class Request {
-public:
-    // Some predefined requests, that are present in every sketch.
-    static const hRequest   HREQUEST_REFERENCE_XY;
-    static const hRequest   HREQUEST_REFERENCE_YZ;
-    static const hRequest   HREQUEST_REFERENCE_ZX;
-
-    int         tag;
-    hRequest    h;
-
-    // Types of requests
-    enum class Type : uint32_t {
-        WORKPLANE              = 100,
-        DATUM_POINT            = 101,
-        LINE_SEGMENT           = 200,
-        CUBIC                  = 300,
-        CUBIC_PERIODIC         = 301,
-        CIRCLE                 = 400,
-        ARC_OF_CIRCLE          = 500,
-        TTF_TEXT               = 600,
-        IMAGE                  = 700
-    };
-
-    Request::Type type;
-    int         extraPoints;
-
-    hEntity     workplane; // or Entity::FREE_IN_3D
-    hGroup      group;
-    hStyle      style;
-
-    bool        construction;
-
-    std::string str;
-    std::string font;
-    Platform::Path file;
-    double      aspectRatio;
-    int groupRequestIndex;
-
-    static hParam AddParam(ParamList *param, hParam hp);
-    void Generate(EntityList *entity, ParamList *param);
-
-    std::string DescriptionString() const;
-    int IndexOfPoint(hEntity he) const;
-
-    void Clear() {}
-};
-
+#include "request.h"
 #include "entitybase.h"
 #include "entity.h"
 
