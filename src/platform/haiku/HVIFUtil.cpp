realtaraharris/solvespace

commit 174042a60de6298735eb685d0a664bbd60895f98

Haiku: clean up HVIF loader

src/platform/haiku/HVIFUtil.cpp
@@ -1,22 +1,26 @@
-// Copyright 2021 pulkomandy <pulkomandy@kitt>, MIT
+/*
+ * Copyright 2023-2025 Tara Harris
+ * <3769985+realtaraharris@users.noreply.github.com> All rights reserved.
+ * Distributed under the terms of the GPLv3 and MIT licenses.
+ */
 
-#include "HVIFUtil.h"
-
-#include "Bitmap.h"
+#include <IconUtils.h>
+#include <Resources.h>
+#include <Bitmap.h>
 
 BBitmap *LoadIconFromResource(const char *name, float iconSize) {
   BResources resources;
   resources.SetToImage(name);
 
-  size_t       size   = 0;
-  const uint8 *buffer = (const uint8 *)resources.LoadResource('VICN', name, &size);
+  size_t size = 0;
+  const uint8 *iconData = (const uint8 *)resources.LoadResource('VICN', name, &size);
 
   if (buffer == NULL) {
     return NULL;
   }
 
   BBitmap *result = new BBitmap(BRect(0, 0, iconSize, iconSize), B_RGBA32);
-  status_t status = BIconUtils::GetVectorIcon(buffer, size, result);
+  status_t status = BIconUtils::GetVectorIcon(iconData, size, result);
   if (status != B_OK) {
     delete result;
     return NULL;

src/platform/haiku/HVIFUtil.h
@@ -1,6 +1,9 @@
+/*
+ * Copyright 2023-2025 Tara Harris
+ * <3769985+realtaraharris@users.noreply.github.com> All rights reserved.
+ * Distributed under the terms of the GPLv3 and MIT licenses.
+ */
+
 #pragma once
 
-#include <IconUtils.h>
-#include <Resources.h>
-
-BBitmap *LoadIconFromResource (const char *name, float iconSize);
+BBitmap *LoadIconFromResource(const char *name, float iconSize);

commit a8cce8880fcd5cb0de37b281b7723c50d00ec0bf

Haiku: fix error introduced in 174042a6

src/platform/haiku/HVIFUtil.cpp
@@ -15,7 +15,7 @@ BBitmap *LoadIconFromResource(const char *name, float iconSize) {
   size_t size = 0;
   const uint8 *iconData = (const uint8 *)resources.LoadResource('VICN', name, &size);
 
-  if (buffer == NULL) {
+  if (iconData == NULL) {
     return NULL;
   }
 